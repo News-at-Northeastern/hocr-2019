b0bad6494b30d99b90a9f4ac8c05856d5bd3a69c
tylermachado <3586466+tylermachado@users.noreply.github.com>
2019-10-16 16:10:11 -0400

    Eunice's changes

--- app/js/plugins/boilerplate.js
+++ app/js/plugins/boilerplate.js
@@ -12,7 +12,231 @@ var map = new mapboxgl.Map({
     interactive: false
 });
 
-// map.scrollZoom.disable();
+map.on('load', function() {
+
+    map.addLayer({
+        "id": "route",
+        "type": "line",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [
+                        [-71.108619, 42.352574],
+                        [-71.110219, 42.352659],
+                        [-71.110601, 42.352667],
+                        [-71.111726, 42.352734],
+                        [-71.113015, 42.352969],
+                        [-71.113968, 42.353410],
+                        [-71.115865, 42.354306],
+                        [-71.116416, 42.354713],
+                        [-71.117055, 42.356493],
+                        [-71.116733, 42.358466],
+                        [-71.116741, 42.361225],
+                        [-71.116862, 42.364230],
+                        [-71.117282, 42.367379],
+                        [-71.118120, 42.368537],
+                        [-71.121695, 42.368643],
+                        [-71.123147, 42.369094],
+                        [-71.124771, 42.369894],
+                        [-71.126291, 42.372248],
+                        [-71.128384, 42.373685],
+                        [-71.130293, 42.374025],
+                        [-71.132979, 42.372978],
+                        [-71.133244, 42.372199],
+                        [-71.132781, 42.371771],
+                        [-71.131795, 42.370321],
+                        [-71.131866, 42.369520],
+                        [-71.132977, 42.368520],
+                        [-71.135380, 42.367429]
+
+                    ]
+                },
+
+            }
+        },
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#b22222",
+            "line-width": 4
+        }
+    });
+
+    map.addLayer({
+        "id": "finish",
+        "type": "line",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {},
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [
+                        [-71.1357789, 42.367916],
+
+                        // [-71.108619, 42.352574],
+                        [-71.135, 42.367058]
+
+                    ]
+                },
+
+            }
+        },
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#ffffff",
+            "line-width": 5
+        }
+    });
+
+    map.addLayer({
+        "id": "start",
+        "type": "line",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "Feature",
+                "properties": {
+
+                    "title": "Start"
+                },
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": [
+                        [-71.108640, 42.353307],
+                        [-71.108630, 42.352055]
+
+                    ]
+                },
+
+            }
+        },
+        "layout": {
+            "line-join": "round",
+            "line-cap": "round"
+        },
+        "paint": {
+            "line-color": "#ffffff",
+            "line-width": 5
+        }
+    });
+
+    // map.loadImage('js/finish_line_icon.png', function(error, image) {
+    //     if (error) throw error;
+    //     map.addImage('finish', image);
+    //     map.addLayer({
+    //     "id": "points",
+    //     "type": "symbol",
+    //     "source": {
+    //         "type": "geojson",
+    //         "data": {
+    //             "type": "FeatureCollection",
+    //             "features": [{
+    //                 "type": "Feature",
+    //                 "geometry": {
+    //                     "type": "Point",
+    //                     "coordinates": [-71.135380, 42.367429]
+    //                 }
+    //             }]
+    //         }
+    //     },
+    //     "layout": {
+    //         "icon-image": "finish",
+    //         "icon-size": 0.25
+    //     }
+    //     });
+    //     });
+
+///Users/eesomonu1994/Desktop/mapbox_sample/app/assets/finish_line_icon.png
+    // map.addLayer({
+    //     "id": "points",
+    //     "type": "symbol",
+    //     "source": {
+    //         "type": "geojson",
+    //         "data": {
+    //             "type": "FeatureCollection",
+    //             "features": [{
+    //                     "type": "Feature",
+    //                     "geometry": {
+    //                         "type": "Point",
+    //                         "coordinates": [-71.108619, 42.352574]
+    //                     },
+    //                     "properties": {
+    //                         "title": "Start",
+    //                         "icon": "embassy"
+    //                     }
+    //                 },
+    //                 {
+    //                     "type": "Feature",
+    //                     "geometry": {
+    //                         "type": "Point",
+    //                         "coordinates": [-71.135380, 42.367429]
+    //                     },
+    //                     "properties": {
+    //                         "title": "End",
+    //                         "icon": "embassy"
+    //                     }
+    //                 }
+    //             ]
+    //         }
+    //     },
+    //     "layout": {
+    //         "icon-image": "{icon}-15",
+    //         "text-field": "{title}",
+    //         "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+    //         "text-offset": [0, 0.6],
+    //         "text-anchor": "top",
+    //
+    //     },
+    //     "paint": {
+    //         "text-color": "#ffffff"
+    //     }
+    // });
+
+
+
+    // map.addSource("source_circle_500", {
+    //     "type": "geojson",
+    //     "data": {
+    //       "type": "FeatureCollection",
+    //       "features": [{
+    //         "type": "Feature",
+    //         "geometry": {
+    //           "type": "Point",
+    //           "coordinates": [-71.1297, 42.363347]
+    //         }
+    //       }]
+    //     }
+    //   });
+    //
+    // map.addLayer({
+    //   "id": "circle500",
+    //   "type": "circle",
+    //   "source": "source_circle_500",
+    //   "paint": {
+    //     "circle-radius": {
+    //       stops: [
+    //         [5, 1],
+    //         [15, s]
+    //       ],
+    //       base: 2
+    //     },
+    //     "circle-color": "red",
+    //     "circle-opacity": 0.6
+    //   }
+    // });
+
+});
 
 var chapters = {
     'overview': {
@@ -57,6 +281,158 @@ var chapters = {
         speed: 0.5
     }
 };
+
+var geojson = {
+  type: 'FeatureCollection',
+  features: [
+      {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-71.1106, 42.3527]
+    },
+    properties: {
+      title: 'Grand Junction Railroad',
+      description: 'Washington, D.C.'
+    }
+},
+{
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: [-71.116648, 42.361392]
+  },
+  properties: {
+    title: 'River Street Bridge'
+  }
+},
+{
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: [-71.116684, 42.364274]
+  },
+  properties: {
+    title: 'Western Avenue Bridge'
+  }
+},
+{
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: [-71.118241, 42.368391]
+  },
+  properties: {
+    title: 'John W. Weeks Footbridge'
+  }
+},
+{
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: [-71.123035, 42.369144]
+  },
+  properties: {
+    title: 'Anderson Memorial Bridge',
+  }
+},
+{
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: [-71.132707, 42.371833]
+  },
+  properties: {
+    title: 'Herter Park Finish Line'
+  }
+}
+]
+};
+//
+// add markers to map
+geojson.features.forEach(function(marker) {
+
+  // create a HTML element for each feature
+  // var el = document.createElement('div');
+  // el.className = 'marker';
+  // var popup = new mapboxgl.Popup()
+  //   .setText(marker.properties.title);
+
+
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker()
+    .setLngLat(marker.geometry.coordinates)
+    .addTo(map);
+    // new mapboxgl.Marker()
+    //     .setLngLat(marker.geometry.coordinates)
+    //
+    //     .addTo(map);
+
+
+
+
+});
+//
+//
+var geojson = {
+    "type": "FeatureCollection",
+    "features": [{
+            "type": "Feature",
+            "properties": {
+                "icon": "start_flags_icon.png",
+                "iconSize": [50, 50],
+                "title": "Start"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-71.108619, 42.352574]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+
+                "icon": "finish_line_icon.png",
+                "iconSize": [50, 50],
+                "title": "End"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-71.135380, 42.367429]
+            }
+        }
+    ],
+        "layout": {
+
+            "text-field": "{title}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+
+        },
+        "paint": {
+            "text-color": "#ffffff"
+        }
+};
+
+// add markers to map
+geojson.features.forEach(function(marker) {
+// create a DOM element for the marker
+var el = document.createElement('div');
+el.className = 'marker';
+el.style.backgroundImage = 'url(js/'+ marker.properties.icon+ ')';
+el.style.width = marker.properties.iconSize[0] + 'px';
+el.style.height = marker.properties.iconSize[1] + 'px';
+
+// el.addEventListener('click', function() {
+// window.alert(marker.properties.message);
+// });
+
+// add marker to map
+new mapboxgl.Marker(el)
+.setLngLat(marker.geometry.coordinates)
+.addTo(map);
+});
 
 // On every scroll event, check which element is on screen
 window.onscroll = function() {
